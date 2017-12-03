d712348020c7a4195bf6e3964c8d86fff34a6458
Roberto Butti <roberto.butti@gmail.com>
2017-12-03 23:23:56 +0100

    Scheduling execution command to retreive tickers

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,8 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        $schedule->command('mycoin:getrocktickers')->everyMinute();
+        //$schedule->command('mycoin:getrocktickers')->everyFiveMinutes();
     }
 
     /**
