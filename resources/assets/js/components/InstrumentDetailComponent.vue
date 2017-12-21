16262e31cadde88c7d0bca55552a6a7ca9db223d
Roberto Butti <roberto.butti@gmail.com>
2017-12-21 23:26:06 +0100

    detail component

--- resources/assets/js/components/InstrumentDetailComponent.vue
+++ resources/assets/js/components/InstrumentDetailComponent.vue
@@ -1,15 +1,54 @@
 <template>
-    <div class="container">
-{{ this.instrument }}
-<p v-for="(item, index) in orderbook_bids">
-    {{ item.amount}}
-    {{ item.depth}}
-    {{ item.price}}
-</p>
-
---------
-
+<div class="container">
+    <div class="columns is- is-marginless">
+        <div class="column is-4">
+            <table class="table is-bordered is-striped is-narrow is-fullwidth">
+            <thead>
+            <tr>
+                <th colspan="3">BID / BUY {{ this.instrument }}</th>
+            </tr>
+            </thead>
+            <tfoot>
+            <tr>
+                <th>Quantity</th>
+                <th>Value</th>
+                <th>Depth</th>
+            </tr>
+            </tfoot>
+            <tbody >
+                <tr v-for="(item, index) in orderbook_bids">
+                     <td>{{ item.price}}</td>
+                    <td>{{ item.amount}}</td>
+                    <td>{{ item.depth}}</td>
+                </tr>
+            </tbody>
+            </table>
+        </div>
+        <div class="column is-4">
+            <table class="table is-bordered is-striped is-narrow is-fullwidth">
+            <thead>
+            <tr>
+                <th colspan="3">ASKS / SELL {{ this.instrument }}</th>
+            </tr>
+            </thead>
+            <tfoot>
+            <tr>
+                <th>Quantity</th>
+                <th>Value</th>
+                <th>Depth</th>
+            </tr>
+            </tfoot>
+            <tbody >
+                <tr v-for="(item, index) in orderbook_asks">
+                     <td>{{ item.price}}</td>
+                    <td>{{ item.amount}}</td>
+                    <td>{{ item.depth}}</td>
+                </tr>
+            </tbody>
+            </table>
+        </div>
     </div>
+</div>
 </template>
 
 <script>
@@ -23,6 +62,7 @@ import Pusher from 'pusher-js' // import Pusher
         },
         data() {
             return {
+                orderbook: [],
                 orderbook_bids: [],
                 orderbook_asks: [],
                 orderbook_loading: false,
@@ -47,14 +87,15 @@ import Pusher from 'pusher-js' // import Pusher
             subscribe () {
                 this.newtrade=[];
                 let pusher = new Pusher('bb1fafdf79a00453b5af');
-                pusher.subscribe('currency')
+                console.log("Subscribe to orderbook:"+this.instrument);
+                pusher.subscribe(this.instrument);
                 
-                pusher.bind('last_trade', data => {
-                    //console.log(data);
-                    console.log(this.newtrade);
-                    this.newtrade.unshift(data /*{ "symbol": data["symbol"], "value": data["value"]}*/);
-                    if (this.newtrade.length >10) {
-                        this.newtrade.pop();
+                pusher.bind('orderbook', data => {
+                    console.log(data);
+                    console.log(this.orderbook);
+                    this.orderbook.unshift(data /*{ "symbol": data["symbol"], "value": data["value"]}*/);
+                    if (this.orderbook.length >10) {
+                        this.orderbook.pop();
                     }
                 });
 
